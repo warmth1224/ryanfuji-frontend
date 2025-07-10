6b156ec80b46adc03b2e88074f2ce78784afed8a
warmth1224 <warmertouch@gmail.com>
2025-07-10 00:56:52 -0400

    first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "RYAN FUJIMOTO",
+  profile: '/profile.png',
+  designation: "Senior Full Stack Engineer Specializing in Solutions for FinTech, HealthTech, EdTech, Digital Publishing, and Online Retail",
+  description: "Hi! I'm Ryan Fujimoto — a full stack engineer who builds fast backends, clean UIs, and sleeps soundly because of test coverage. I speak JavaScript fluently, automate the boring stuff, and ship code that scales (and rarely cries in prod). 9+ years in FinTech, SaaS, and keeping CI green. Let’s build something solid.",
+  email: 'worldwizad0303@gmail.com',
+  phone: '+14074369174',
+  address: 'Osaka, Japan',
+  github: 'https://github.com/caraX0',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/masayoshi-yamada-7bba82372/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume: "https://drive.google.com/file/d/1Mjst9wCRirSkXkVYEo1jGZC4ZILhz1-c/view?usp=drive_link"
+}
